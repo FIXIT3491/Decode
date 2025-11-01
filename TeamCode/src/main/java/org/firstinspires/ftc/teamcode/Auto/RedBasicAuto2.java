bc12c1edebb91944f44324928b5c865a517489bb
Ryan Alder <rryan.alder88@gmail.com>
2025-11-01 08:57:10 -0700

    outline for back blue and red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedBasicAuto2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedBasicAuto2.java
@@ -19,7 +19,46 @@ public class RedBasicAuto2 extends LinearOpMode {
 
         launcher.init(hardwareMap);
 
+        //drive forward to shooting zone and turn to shoot
+        launcher.closeGate();
+        sleep(1000);
+        drive.drive(-0.45,0,0);
+        sleep(1200);
+        drive.drive(0,0,0);
+        drive.turnToHeading(45,0.45, 0.25);
+        drive.resetHeading();
+        sleep(400);
+        /*
+        launcher.setFlywheelVelocity(1800);
+        sleep(1500);
+        launcher.openGate();
+        sleep(2500);
+        launcher.stop();
+        launcher.closeGate();
+        sleep(1500);
 
+        //turn and sweep
+        drive.drive(-0.45,0,0);
+        sleep(1125);
+        drive.drive(0,0,0);
+        sleep(400);
+        drive.turnToHeading(-35,0.45,0.24);
+        sleep(1000);
+        drive.resetHeading();
+        drive.drive(0.5,0,0);
+        sleep(1400);
+        drive.drive(0,0,0);
+        sleep(400);
+
+        //turn and move back slightly
+        drive.turnToHeading(45,0.45,0.5);
+        sleep(1000);
+        drive.resetHeading();
+        drive.drive(-0.45,0,0);
+        sleep(500);
+        drive.drive(0,0,0);
+        sleep(400);
+        */
 
     }
 }
