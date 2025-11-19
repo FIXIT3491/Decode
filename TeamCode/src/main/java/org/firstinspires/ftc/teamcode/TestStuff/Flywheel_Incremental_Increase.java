9b7f7cf5bf74887e587b192fca50cc17a5a6be31
Ryan Alder <rryan.alder88@gmail.com>
2025-11-18 19:37:45 -0800

    Finalized Flywheel speed test increase and decrease

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestStuff/Flywheel_Incremental_Increase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestStuff/Flywheel_Incremental_Increase.java
@@ -6,56 +6,58 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Commands.Launcher;
 
 @TeleOp(group = "Test", name = "Flywheel Increase")
-public class Flywheel_Incremental_Increase extends OpMode{
+public class Flywheel_Incremental_Increase extends OpMode {
+
+    double power = 0.5;
 
     Launcher launcher = new Launcher();
 
+    // Previous button states (for debouncing)
+    boolean prevDpadLeft = false;
+    boolean prevDpadRight = false;
+    boolean prevRB = false;
+    boolean prevLB = false;
+
     @Override
-    public void init(){
-
-        //Init Launcher Subsystem
+    public void init() {
         launcher.init(hardwareMap);
-
     }
 
     @Override
     public void loop() {
 
-        double power = 0.5;
-
-        //adjusments of power for flywheel testing
-        if (gamepad1.dpad_left) {
-
+        // decrease 0.01
+        if (gamepad1.dpad_left && !prevDpadLeft) {
             power -= 0.01;
+        }
+        prevDpadLeft = gamepad1.dpad_left;
 
-        } else if (gamepad1.dpad_right) {
-
+        //increase 0.01
+        if (gamepad1.dpad_right && !prevDpadRight) {
             power += 0.01;
-
-        } else if (gamepad1.right_bumper) {
-
-            power += 0.1;
-
-        } else if (gamepad1.left_bumper) {
-
-            power += 0.1;
-
         }
+        prevDpadRight = gamepad1.dpad_right;
 
-        //Turning on flywheel
+        //increase 0.1
+        if (gamepad1.right_bumper && !prevRB) {
+            power += 0.1;
+        }
+        prevRB = gamepad1.right_bumper;
+
+        //decrease 0.1
+        if (gamepad1.left_bumper && !prevLB) {
+            power -= 0.1;
+        }
+        prevLB = gamepad1.left_bumper;
+
+        //Flywheel controls
         if (gamepad1.b) {
-
             launcher.setFlywheelRPM(power);
-
         } else {
-
             launcher.stop();
-
         }
 
-        telemetry.addData("Percentage:", power);
+        telemetry.addData("Flywheel Power:", power);
         telemetry.update();
-
     }
-
 }
