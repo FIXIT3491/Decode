d248eef0c3f63371c136c839e307a2d747cff737
Ryan Alder <rryan.alder88@gmail.com>
2026-02-04 22:11:52 -0800

    Intake cycle automation for the reverse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestStuff/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestStuff/IntakeTest.java
@@ -19,12 +19,15 @@ public class IntakeTest extends OpMode {
     private DcMotor intake;
 
     private boolean lastA = false;
+    private boolean lastB = false;
     private boolean lastTrigger = false;
     boolean intakeInitialized = false;
-
-    private boolean last2Trigger = false;
+    boolean wheelAtIdle = true;
     private double intakeCounter = 0;
-    private double intake2Counter = 0;
+    private boolean lastOuttakeTrigger = false;
+    private boolean outtakeInitialized = false;
+    private boolean outtakeWheelAtIdle = true;
+    private int outtakeCounter = 0;
 
     private WheelRotation wheel = new WheelRotation();
 
@@ -50,51 +53,99 @@ public class IntakeTest extends OpMode {
         drive.drive(y, x, rx);
 
         boolean triggerPressed = gamepad1.right_trigger > 0.1;
+        boolean outtakePressed = gamepad1.left_trigger > 0.1;
         boolean aPressed = gamepad1.a;
+        boolean bPressed = gamepad1.b;
 
-        // Intake runs continuously while trigger is held
         if (triggerPressed) {
 
             intake.setPower(0.8);
 
+        } else if (outtakePressed){
+
+            intake.setPower(-0.8);
+
         } else {
 
             intake.setPower(0.0);
-            intakeInitialized = false; // reset when trigger released
-            intakeCounter = 0;         // optional: reset cycle when trigger released
 
         }
 
-        // First trigger press -> run first rotateToAngle
-        if (triggerPressed && !lastTrigger && !intakeInitialized) {
+        if (triggerPressed && !lastTrigger) {
 
-            wheel.rotateToAngle(30, 0.3);
-            intakeCounter = 1; // next A press goes to second angle
+            wheel.rotateToAngle(35, 0.3);
+            intakeCounter = 1;
             intakeInitialized = true;
+            wheelAtIdle = false;
 
         }
 
-        // A button cycles after initialization
         if (aPressed && !lastA && intakeInitialized) {
             switch ((int) intakeCounter) {
                 case 0:
-                    wheel.rotateToAngle(30, 0.3);
+                    wheel.rotateToAngle(35, 0.3);
                     break;
                 case 1:
-                    wheel.rotateToAngle(150, 0.3);
+                    wheel.rotateToAngle(148, 0.3);
                     break;
                 case 2:
-                    wheel.rotateToAngle(290, 0.3);
+                    wheel.rotateToAngle(268, 0.3);
                     break;
             }
 
             intakeCounter = (intakeCounter + 1) % 3;
-
+            wheelAtIdle = false;
         }
 
-        // Save previous states
+        if (!triggerPressed && !wheelAtIdle) {
+
+            wheel.rotateToAngle(358, 0.3);
+            wheelAtIdle = true;
+
+            // reset for next cycle
+            intakeInitialized = false;
+            intakeCounter = 0;
+        }
+
         lastTrigger = triggerPressed;
         lastA = aPressed;
+
+        //Outtake
+
+        if (outtakePressed && !lastOuttakeTrigger) {
+            wheel.rotateToAngle(90, 0.3);
+            outtakeCounter = 1;
+            outtakeInitialized = true;
+            outtakeWheelAtIdle = false;
+        }
+
+        if (bPressed && !lastB && outtakeInitialized) {
+            switch (outtakeCounter) {
+                case 0:
+                    wheel.rotateToAngle(90, 0.3);
+                    break;
+                case 1:
+                    wheel.rotateToAngle(200, 0.3);
+                    break;
+                case 2:
+                    wheel.rotateToAngle(320, 0.3);
+                    break;
+            }
+
+            outtakeCounter = (outtakeCounter + 1) % 3;
+            outtakeWheelAtIdle = false;
+        }
+
+        if (!outtakePressed && !outtakeWheelAtIdle) {
+            wheel.rotateToAngle(358, 0.3);
+            outtakeWheelAtIdle = true;
+
+            outtakeInitialized = false;
+            outtakeCounter = 0;
+        }
+
+        lastOuttakeTrigger = outtakePressed;
+        lastB = bPressed;
 
 
 /*
@@ -124,12 +175,6 @@ public class IntakeTest extends OpMode {
             lastA = aPressed;
 
         }  */
-
-        if (gamepad1.left_trigger > 0) {
-            intake.setPower(-0.8);
-        } else {
-            intake.setPower(0);
-        }
 
     /*
         boolean aPressed = gamepad1.a;
