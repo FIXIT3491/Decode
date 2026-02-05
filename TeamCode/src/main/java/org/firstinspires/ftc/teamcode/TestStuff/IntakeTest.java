bb1cb8faa83f65be325b2977e125255f3556cbff
Ryan Alder <rryan.alder88@gmail.com>
2026-02-04 19:35:21 -0800

    Intake cycle automation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestStuff/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestStuff/IntakeTest.java
@@ -19,7 +19,12 @@ public class IntakeTest extends OpMode {
     private DcMotor intake;
 
     private boolean lastA = false;
+    private boolean lastTrigger = false;
+    boolean intakeInitialized = false;
+
+    private boolean last2Trigger = false;
     private double intakeCounter = 0;
+    private double intake2Counter = 0;
 
     private WheelRotation wheel = new WheelRotation();
 
@@ -44,14 +49,89 @@ public class IntakeTest extends OpMode {
 
         drive.drive(y, x, rx);
 
-        if (gamepad1.right_trigger > 0) {
+        boolean triggerPressed = gamepad1.right_trigger > 0.1;
+        boolean aPressed = gamepad1.a;
+
+        // Intake runs continuously while trigger is held
+        if (triggerPressed) {
+
             intake.setPower(0.8);
-        } else if (gamepad1.left_trigger > 0) {
+
+        } else {
+
+            intake.setPower(0.0);
+            intakeInitialized = false; // reset when trigger released
+            intakeCounter = 0;         // optional: reset cycle when trigger released
+
+        }
+
+        // First trigger press -> run first rotateToAngle
+        if (triggerPressed && !lastTrigger && !intakeInitialized) {
+
+            wheel.rotateToAngle(30, 0.3);
+            intakeCounter = 1; // next A press goes to second angle
+            intakeInitialized = true;
+
+        }
+
+        // A button cycles after initialization
+        if (aPressed && !lastA && intakeInitialized) {
+            switch ((int) intakeCounter) {
+                case 0:
+                    wheel.rotateToAngle(30, 0.3);
+                    break;
+                case 1:
+                    wheel.rotateToAngle(150, 0.3);
+                    break;
+                case 2:
+                    wheel.rotateToAngle(290, 0.3);
+                    break;
+            }
+
+            intakeCounter = (intakeCounter + 1) % 3;
+
+        }
+
+        // Save previous states
+        lastTrigger = triggerPressed;
+        lastA = aPressed;
+
+
+/*
+        while (gamepad1.right_trigger > 0) { // was an if so if it doesnt work swap back
+            intake.setPower(0.8);
+
+            boolean aPressed = gamepad1.a;
+
+            if (aPressed && !lastA) {
+
+                switch ((int) intakeCounter) {
+                    case 0:
+                        wheel.rotateToAngle(30, 0.3);
+                        break;
+                    case 1:
+                        wheel.rotateToAngle(150, 0.3);
+                        break;
+                    case 2:
+                        wheel.rotateToAngle(290, 0.3);
+                        break;
+                }
+
+                intakeCounter = (intakeCounter + 1) % 3;
+
+            }
+
+            lastA = aPressed;
+
+        }  */
+
+        if (gamepad1.left_trigger > 0) {
             intake.setPower(-0.8);
         } else {
             intake.setPower(0);
         }
 
+    /*
         boolean aPressed = gamepad1.a;
 
         if (aPressed && !lastA) {
@@ -72,7 +152,7 @@ public class IntakeTest extends OpMode {
 
         }
 
-        lastA = aPressed;
+        lastA = aPressed; */
 
         wheel.updateTelemetry();
 
